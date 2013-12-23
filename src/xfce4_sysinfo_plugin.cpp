c6e671f72c99e148f2790555ea676df88cdd5402
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2013-12-23 19:23:55 +1100

    initial source

---
+++ src/xfce4_sysinfo_plugin.cpp
@@ -0,0 +1,24 @@
+/* xfce4-sysinfo-plugin
+   Copyright (C) 2013 Jarryd Beck
+
+This file is part of xfce4-sysinfo-plugin.
+
+xfce4-sysinfo-plugin is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+xfce4-sysinfo-plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with xfce4-sysinfo-plugin; see the file COPYING.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+int
+main(int argc, char* argv[])
+{
+  return 0;
+}
