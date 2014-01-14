610700c3f23db15ccbf56101f3ff7cea422a881c
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2014-01-14 16:30:56 +1100

    add tester.c

---
+++ src/tester.c
@@ -0,0 +1,53 @@
+#include "xfce4-sysinfo-plugin/xfce4_sysinfo_plugin.h"
+#include "xfce4-sysinfo-plugin/plugins.h"
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+  sysinfo_initialise();
+
+  SysinfoPluginList* plugins = sysinfo_load_plugins();
+
+  size_t size = sysinfo_pluginlist_size(plugins);
+
+  printf("Found %zu plugins\n", size);
+
+  size_t i = 0;
+  while (i != size)
+  {
+    SysinfoPlugin* p = sysinfo_pluginlist_get(plugins, i);
+    printf("%s\n", p->plugin_name);
+    ++i;
+  }
+
+  printf("Printing data every second, press ^C to quit\n");
+
+  while (1)
+  {
+    sleep(1);
+    i = 0;
+    while (i != size)
+    {
+      SysinfoPlugin* p = sysinfo_pluginlist_get(plugins, i);
+      printf("%s\n", p->plugin_name);
+
+      SysinfoPluginData data;
+
+      (*p->get_data)(p, &data);
+
+      size_t j = 0;
+      while (j != p->num_data)
+      {
+        printf("%g\t", data.data[j]);
+        ++j;
+      }
+
+      printf("\n");
+
+      ++i;
+    }
+  }
+
+  return 0;
+}
