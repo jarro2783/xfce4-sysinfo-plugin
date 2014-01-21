a62397ba777882ac50611e17029c5afae86af8fb
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2014-01-21 17:34:20 +1100

    starting netload

--- plugins/net/sysinfo_net.c
+++ plugins/net/sysinfo_net.c
@@ -20,10 +20,68 @@ along with xfce4-sysinfo-plugin; see the file COPYING.  If not see
 
 */
 
-#include <glibtop/cpu.h>
+#include <glibtop/netlist.h>
 #include <math.h>
 
 #include "xfce4-sysinfo-plugin/plugins.h"
+
+#define DATA_FIELDS 3
+
+enum
+{
+  NET_IN,
+  NET_OUT,
+  NET_LOCAL
+};
+
+static gchar* data_names[DATA_FIELDS] =
+{
+  "In",
+  "Out",
+  "Local"
+};
+
+typedef struct
+{
+  gchar** interfaces;
+  int num_interfaces;
+} NetData;
+
+static void
+init_color(SysinfoPlugin* plugin)
+{
+  SysinfoColor* c = g_new(SysinfoColor, DATA_FIELDS);
+
+  c[NET_IN].red = 0xff / 255.;
+  c[NET_IN].green = 0xe8 / 255.;
+  c[NET_IN].blue = 0;
+
+  c[NET_OUT].red = 0xff / 255.;
+  c[NET_OUT].green = 0xf2 / 255.;
+  c[NET_OUT].blue = 0x73 / 255.;
+
+  c[NET_LOCAL].red = 0xa6 / 255.;
+  c[NET_LOCAL].green = 0x97 / 255.;
+  c[NET_LOCAL].blue = 0;
+
+  plugin->colors = c;
+}
+
+static void
+get_interfaces(NetData* data)
+{
+  glibtop_netlist list;
+  data->interfaces = glibtop_get_netlist(&list);
+  data->num_interfaces = list.number;
+}
+
+static void
+close(SysinfoPlugin* plugin)
+{
+  NetData* data = (NetData*)plugin->plugin_data;
+
+  g_strfreev(data->interfaces);
+}
 
 SysinfoPlugin*
 sysinfo_data_plugin_init()
@@ -31,6 +89,13 @@ sysinfo_data_plugin_init()
   SysinfoPlugin* plugin = g_new(SysinfoPlugin, 1);
 
   plugin->plugin_name = "Net Load";
+  plugin->num_data = DATA_FIELDS;
+  plugin->data_names = data_names;
+
+  init_color(plugin);
+
+  NetData* data = g_new(NetData, 1);
+  plugin->plugin_data = data;
 
   return plugin;
 }
